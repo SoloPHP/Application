8d9e77a4d3ec01d08d62211be65762340898f642
SoloPHP <vedastudio@gmail.com>
2024-12-15 15:20:13 +0200

    v1.1.0

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
-# Application
-PSR compatible PHP Application with Router
+# Solo Application
+
+[![Latest Version](https://img.shields.io/github/release/solophp/application.svg)](https://github.com/solophp/application/releases)
+[![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg)](LICENSE)
+
+A PSR compliant application class with middleware support and routing capabilities.
+
+## Requirements
+
+- PHP 8.1 or higher
+- PSR-7 HTTP message interfaces implementation
+- PSR-11 container implementation
+- PSR-17 HTTP factory implementation
+
+## Installation
+
+Install via Composer:
+
+```bash
+composer require solophp/application
+```
+
+## Dependencies
+
+- [psr/container](https://github.com/php-fig/container) ^2.0
+- [psr/http-message](https://github.com/php-fig/http-message) ^2.0
+- [psr/http-server-handler](https://github.com/php-fig/http-server-handler) ^1.0
+- [psr/http-factory](https://github.com/php-fig/http-factory) ^1.0
+- [solophp/router](https://github.com/solophp/router) ^1.0
+
+## Basic Usage
+
+```php
+use Solo\Application;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+$app = new Application($container);
+
+// Add route
+$app->get('/hello/{name}', function (ServerRequestInterface $request, ResponseInterface $response, array $args) {
+    $response->getBody()->write("Hello, {$args['name']}!");
+    return $response;
+});
+
+// Add middleware
+$app->addMiddleware(SomeMiddleware::class);
+
+// Run application
+$response = $app->run($request);
+```
+
+## Constructor Options
+
+```php
+public function __construct(
+    ContainerInterface $container,
+    ?ResponseFactoryInterface $responseFactory = null
+)
+```
+
+- `$container` - PSR-11 container implementation
+- `$responseFactory` - Optional PSR-17 response factory. If not provided, will be retrieved from container.
+
+## Middleware
+
+Add middleware using the `addMiddleware()` method:
+
+```php
+$app->addMiddleware(CorsMiddleware::class);
+$app->addMiddleware(new AuthMiddleware($container));
+```
+
+Middleware can be added as either:
+- Class name (string)
+- Object instance
+
+Class name middleware will be instantiated via container.
+
+## Routing
+
+The application extends `RouteCollector` and provides standard routing methods:
+
+```php
+// Route with callback
+$app->get('/api/users', function ($request, $response) {
+    // Handle request
+    return $response;
+});
+
+// Route with controller
+$app->post('/api/users', [UserController::class, 'create']);
+
+// Route with middleware
+$app->get('/admin/dashboard', [DashboardController::class, 'index'])
+    ->addMiddleware(AdminAuthMiddleware::class);
+```
+
+## Exception Handling
+
+The following exceptions may be thrown:
+
+- `ContainerExceptionInterface` - Error retrieving service from container
+- `NotFoundExceptionInterface` - Service not found in container
+- `InvalidArgumentException` - Invalid route handler
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE) for more information.
--- Solo/Application.php
+++ Solo/Application.php
@@ -3,40 +3,62 @@
 namespace Solo;
 
 use Solo\Router\RouteCollector;
-use Nyholm\Psr7\Factory\Psr17Factory;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-
 use InvalidArgumentException;
 
 class Application extends RouteCollector implements RequestHandlerInterface
 {
     private int $index = 0;
     private array $middlewares = [];
-    private Psr17Factory $psr17Factory;
+    private ResponseFactoryInterface $responseFactory;
     private ContainerInterface $container;
 
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
     public function __construct(
-        ContainerInterface $container
+        ContainerInterface        $container,
+        ?ResponseFactoryInterface $responseFactory = null
     )
     {
         $this->container = $container;
-        $this->psr17Factory = $this->container->get(Psr17Factory::class);
+        $this->responseFactory = $responseFactory ?? $container->get(ResponseFactoryInterface::class);
     }
 
-    public function addMiddleware($middleware): void
+    /**
+     * Adds middleware to the application pipeline.
+     *
+     * @param object|string $middleware Middleware class name or instance
+     */
+    public function addMiddleware(object|string $middleware): void
     {
-        $this->middlewares[] = is_string($middleware) ? new $middleware($this->container) : $middleware;
+        $this->middlewares[] = is_string($middleware)
+            ? new $middleware($this->container)
+            : $middleware;
     }
 
+    /**
+     * Runs the application with the given request.
+     *
+     * @param ServerRequestInterface $request PSR-7 server request
+     * @return ResponseInterface PSR-7 response
+     * @throws ContainerExceptionInterface if there was an error while retrieving the service
+     * @throws NotFoundExceptionInterface no entry was found for the identifier
+     * @throws InvalidArgumentException when route handler is invalid
+     */
     public function run(ServerRequestInterface $request): ResponseInterface
     {
         $routeInfo = $this->matchRoute($request->getMethod(), $request->getUri()->getPath());
 
         if ($routeInfo === false) {
-            return $this->psr17Factory->createResponse(404);
+            return $this->responseFactory->createResponse(404);
         }
 
         array_map([$this, 'addMiddleware'], $routeInfo['middleware']);
@@ -46,6 +68,15 @@ class Application extends RouteCollector implements RequestHandlerInterface
         return $this->handle($request);
     }
 
+    /**
+     * Handles the request through middleware pipeline.
+     *
+     * @param ServerRequestInterface $request PSR-7 server request
+     * @return ResponseInterface PSR-7 response
+     * @throws ContainerExceptionInterface if there was an error while retrieving the service
+     * @throws NotFoundExceptionInterface no entry was found for the identifier
+     * @throws InvalidArgumentException when route handler is invalid
+     */
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         if (!isset($this->middlewares[$this->index])) {
@@ -55,20 +86,31 @@ class Application extends RouteCollector implements RequestHandlerInterface
         return $this->middlewares[$this->index - 1]->process($request, $this);
     }
 
+    /**
+     * Dispatches the route handler.
+     *
+     * @param ServerRequestInterface $request PSR-7 server request
+     * @return ResponseInterface PSR-7 response
+     * @throws InvalidArgumentException when handler is invalid
+     * @throws ContainerExceptionInterface if there was an error while retrieving the service
+     * @throws NotFoundExceptionInterface no entry was found for the identifier
+     */
     private function routeDispatcher(ServerRequestInterface $request): ResponseInterface
     {
         $routeInfo = $request->getAttribute('routeInfo');
-        $response = $this->psr17Factory->createResponse();
+        $response = $this->responseFactory->createResponse();
         $handler = $routeInfo['handler'];
 
         if (is_array($handler) && count($handler) === 2) {
             $controller = $this->container->get($handler[0]);
             $method = $handler[1];
             return $controller->$method($request, $response, $routeInfo['args']);
-        } elseif (is_callable($handler)) {
-            return call_user_func($handler, $request, $response, $routeInfo['args']);
-        } else {
-            throw new InvalidArgumentException('Invalid route handler');
         }
+
+        if (is_callable($handler)) {
+            return $handler($request, $response, $routeInfo['args']);
+        }
+
+        throw new InvalidArgumentException('Invalid route handler');
     }
 }
